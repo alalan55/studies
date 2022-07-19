cc8be4f5723db8f30961c779002d972fa11122f8
aoliveira <aoliveira@keener.io>
2022-07-19 19:37:32 -0300

    Insere informações no componente e estilizaçãoi do mesmo

---
+++ src/components/templates/DataProvider/components/placeCard.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="card">
+    <div class="card__loading" v-if="loading">
+      <div class="card__loading__message">
+        <span> ... Buscando dados </span>
+      </div>
+    </div>
+
+    <div class="card__wrapper" v-else>
+      <ul>
+        <li class="item">
+          <span> <strong>Estado:</strong> {{ info.state }} </span>
+        </li>
+        <li class="item">
+          <span> <strong>Pais:</strong> {{ info.country }} </span>
+        </li>
+        <li class="item">
+          <span> <strong>Cidade:</strong> {{ info.city }} </span>
+        </li>
+        <li class="item">
+          <span> <strong>Rua:</strong> {{ info.street_address }} </span>
+        </li>
+        <li class="item">
+          <span> <strong>Endereço completo:</strong> {{ info.full_address }} </span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    info: { type: Object },
+    loading: { type: Boolean, default: false },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/utilities";
+.card {
+  &__loading {
+    border-radius: 10px;
+    text-align: center;
+    background: $blue-4;
+    &__message {
+      padding: $s1;
+      span {
+        font-weight: 600;
+        color: $blue-0;
+        font-size: 1.1em;
+      }
+    }
+  }
+  &__wrapper {
+    background: $blue-4;
+    padding: $s1;
+    margin: $s2 0;
+    border-radius: 10px;
+
+    ul {
+      padding: 0;
+      li {
+        list-style: none;
+        margin: $s2 0;
+
+        span {
+          strong {
+            color: $blue-0;
+            font-size: 1.1em;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/templates/DataProvider/dataProvider.vue
+++ src/components/templates/DataProvider/dataProvider.vue
@@ -1,16 +1,26 @@
 <template>
-  <templateWrapper title="Data provider" :description="text"> </templateWrapper>
+  <templateWrapper title="Data provider" :description="text">
+    <!-- IMPORTAÇÃO DO COMPONENTE PROVEDOR E CHAMA O SLOT DEFAULT CRIADO DESESTRUTURANDO OS DADOS RECEBIDOS -->
+    <providerComponent :url="url" v-slot="{ res: response, loading }">
+      <place-card :info="response" :loading="loading" />
+    </providerComponent>
+  </templateWrapper>
 </template>
 
 <script>
 import templateWrapper from "@/components/bosons/templateWrapper.vue";
+import providerComponent from "./components/providerComponent.vue";
+import placeCard from "./components/placeCard.vue";
 export default {
   components: {
     templateWrapper,
+    providerComponent,
+    placeCard,
   },
   setup() {
+    const url = "https://random-data-api.com/api/address/random_address";
     const text = `Estudo sobre data provider component. Consumo de api para pegar um lugar diferente, utilizando o concecito de data provider pattern.`;
-    return { text };
+    return { text, url };
   },
 };
 </script>
