067b43c286639c336b803d6fc2e461d5ce4fd5e2
aoliveira <aoliveira@keener.io>
2023-03-07 11:38:17 -0300

    Inicia construção da página de drag and drop

---
+++ src/components/templates/DragAndDrop/dragADrop.vue
@@ -0,0 +1,11 @@
+<script setup>
+import templateWrapper from "../../bosons/templateWrapper.vue";
+</script>
+
+<template>
+  <templateWrapper title="Drag and Drop" description="teste">
+    <h1>teste</h1>
+  </templateWrapper>
+</template>
+
+<style lang="scss" scoped></style>
