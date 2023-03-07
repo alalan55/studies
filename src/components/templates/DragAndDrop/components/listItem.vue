45a4808c35c0e6f78480ad5da7097a3526aa5a95
aoliveira <aoliveira@keener.io>
2023-03-07 12:22:49 -0300

    Insere criação de componentes iniciais básicos

---
+++ src/components/templates/DragAndDrop/components/listItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="list-item">
+    <div class="list-item__header" @click="toggleShow()">
+      <span> {{ show ? "-" : "+" }} {{ props.info.name }} </span>
+    </div>
+    <div v-if="show" class="list-item__body">
+      <ul>
+        <li v-for="(item, i) in props.info.rows" :key="i">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+    <!-- {{ props }} -->
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  info: { type: Object, default: null },
+});
+
+const show = ref(false);
+
+const toggleShow = () => (show.value = !show.value);
+</script>
+
+<style lang="scss" scoped>
+.list-item {
+  padding: 0.6rem 0;
+  &__header {
+    cursor: pointer;
+  }
+
+  &__body {
+    margin: 1rem 0 0 1rem;
+    ul {
+      padding: 0;
+
+      li {
+        list-style: none;
+      }
+    }
+  }
+}
+</style>
--- src/components/templates/DragAndDrop/dragADrop.vue
+++ src/components/templates/DragAndDrop/dragADrop.vue
@@ -1,11 +1,95 @@
 <script setup>
+import { ref } from "vue";
+// import draggable from "vuedraggable";
+import listItem from "./components/listItem.vue";
 import templateWrapper from "../../bosons/templateWrapper.vue";
+
+const text = "Estudo sobre drag and drop, afim de implementação real de query builder";
+const columns = ref([
+  {
+    name: "ID",
+    rows: [
+      {
+        name: "Número",
+        value: "",
+      },
+    ],
+  },
+  {
+    name: "CPF",
+    rows: [
+      {
+        name: "Número",
+        value: "",
+      },
+    ],
+  },
+  {
+    name: "CEP",
+    rows: [
+      {
+        name: "Número",
+        value: "",
+      },
+    ],
+  },
+  {
+    name: "GENERO",
+    rows: [
+      {
+        name: "Número",
+        value: "",
+      },
+    ],
+  },
+  {
+    name: "EMAIL",
+    rows: [
+      {
+        name: "Número",
+        value: "",
+      },
+    ],
+  },
+]);
 </script>
 
 <template>
-  <templateWrapper title="Drag and Drop" description="teste">
-    <h1>teste</h1>
+  <templateWrapper title="Drag and Drop" :description="text">
+    <div class="content">
+      <div class="content__left">
+        <div class="list">
+          <listItem
+            v-for="(item, i) in columns"
+            :key="i"
+            :info="item"
+            class="list__item"
+          />
+        </div>
+      </div>
+      <div class="content__right"></div>
+    </div>
   </templateWrapper>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/scss/main.scss";
+
+.content {
+  display: flex;
+  & > div {
+    flex: 1 1 500px;
+    border: 1px solid;
+  }
+
+  &__left {
+    .list {
+      &__item {
+        border-bottom: 1px solid;
+      }
+    }
+  }
+  &__right {
+  }
+}
+</style>
