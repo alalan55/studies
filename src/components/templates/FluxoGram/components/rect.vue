edc3ce8754836a07bec63b83792a93b78f56b6f2
alalan55 <alla.ao4975@gmail.com>
2023-04-20 10:47:45 -0300

    Inicia construção de elementos basicos

---
+++ src/components/templates/FluxoGram/components/rect.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<script>
+export default {
+  name: "Rect",
+};
+</script>
+<template>
+  <div class="rect"></div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/main.scss";
+
+.rect{
+    border: 1px solid;
+}
+</style>
--- src/components/templates/FluxoGram/index.vue
+++ src/components/templates/FluxoGram/index.vue
@@ -1,15 +1,41 @@
 <script setup>
-
+import { ref } from "vue";
+import Rect from "./components/rect.vue";
+const nodes = ref([
+  {
+    id: 1,
+    tipo: "node",
+    x: 0,
+    y: 0,
+    texto: "",
+    conexoes: [],
+  },
+]);
 </script>
 
-
 <template>
-    <div>
-        <h1>Página de fluxograma</h1>
+  <div class="flow">
+    <h1>Página de fluxograma</h1>
+
+    <div class="flow__list">
+        hey
     </div>
+  </div>
 </template>
 
-
 <style lang="scss" scoped>
+@import "@/assets/scss/main.scss";
 
+.flow {
+  height: calc(100vh - $navHeight - 40px);
+  display: flex;
+  flex-direction: column;
+
+  &__list{
+    border: 1px solid;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+}
 </style>
